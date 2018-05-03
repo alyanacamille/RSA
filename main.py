227c718b995abd85b96995403a2ebd5721ff93a6
Alyana W <31449636+alyanacamille@users.noreply.github.com>
2018-05-02 18:40:13 -0700

    Final Changes?????

--- main.py
+++ main.py
@@ -1,66 +1,48 @@
-#keys#
-public_key = (21721, 113)
-private_key = (21412, 14401)
-encrypted_message = ("")
-decrypted_message = ("")
 
-message = 'Seven Nation Army'
+#encrypt#
+def encryption(e, n):
+	encrypted_message = ""
+	message = input("what would you like to encrypt: ")
+	for x in message:
+		numerize = ord(x)
+		encrypt = pow(numerize, e, n)
+		denumerize = chr(encrypt)
+		encrypted_message += denumerize
+	print(encrypted_message)
+	print("")
+	RSA()
 
-e = ("")
-n = ("")
-d = ("")
-
-#starting message#
-def rsa():
-    print ("Would you like to encrypt or decrypt?")
-    print ("")
-    print ("type 'e' to encrypt or 'd' to decrypt.")
-    answer = input()
-    if answer == 'e' or answer == 'E':
-        print ("")
-        print ("you chose to encrypt")
-        print ("")
-        encrypt(e,n)
-        
-    elif answer == 'd' or answer == 'D':
-        print ("")
-        print ("you chose to decrypt")
-        print ("")
-        decrypt(d,n)
-    else:
-        print ("")
-        print ("answer not found, try again")
-        rsa()
-  
-#encryption function#
-
-def encrypt(e,n):
-	print ("to encrypt please give the following:")
- 	e = input("enter 'e' value: ")
-	n = input("enter 'n' value: ")   
-	print ("") 
-	print ("enter the message you would like to encrypt")
- 
-answer = input()
-
-for answer in input():
- numerize = ord(answer)
- encrypt = pow(numerize, e, n)
- denumerize = chr(encrypt)
- encrypted_message += denumerize
-
-print encrypted_message
-
-###decryption funcation###
-def decrypt(d, n):
-  encrypted_message = ''
-  decrypted_message = ''
-  message = input('What would you like to decrypt?')
-  for t in message:
-    numerize = ord(t)
-    decrypt = pow(numerize, d, n)
-    denumerize = chr(decrypt)
-    decrypted_message += denumerize
-  print(decrypted_message)
-  #########
-rsa()
+#decrypt#
+def decryption(d, n):
+	decrypted_message = ""
+	emessage = input("what would you like to decrypt: ")
+	for t in emessage:
+		numerize = ord(t)
+		decrypt = pow(numerize, d, n)
+		denumerize = chr(decrypt)
+		decrypted_message += denumerize
+	print(decrypted_message)
+	print("")
+	
+def RSA():
+	print("what would you like to do?")
+	print("type 'encrypt' to encrypt a message")
+	print("type 'decrypt' to decrypt a message")
+	print("type 'leave' to leave")
+	choice = input("your response: ")
+	while input != choice:
+		if choice == "encrypt":
+			e = int(input("your 'e' # : "))
+			n = int(input("your 'n' # : "))
+			encryption(n, e)
+		if choice == "decrypt":
+			d = int(input("your 'd' # : "))
+			PhiN = int(input("enter your 'n' # : "))
+			decryption(d, PhiN)
+		if choice == "leave":
+			break
+		else:
+			RSA()
+			break
+			
+	RSA()
